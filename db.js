c98175692ae34f138b785ee41c1363a1593f1b52
anurag <anuragbadwahe@gmail.com>
2023-11-07 18:13:30 +0530

    Fix bugs of node and mongoose

--- db.js
+++ db.js
@@ -1,17 +1,39 @@
-const MONGO_URL = process.env.MONGO_URL
+// const MONGO_URL = process.env.MONGO_URL
 
+// const mongoose = require("mongoose");
+
+// mongoose.set("strictQuery", true);
+
+// const connectDB = () => {
+//   mongoose
+//     .connect(MONGO_URL, {})
+//     .then(() => {
+//       console.log("Connected to Mongo successful");
+//     })
+//     .catch((e) => {
+//       console.log(e.message);
+//     });
+// };
+// module.exports = connectDB;
+
+const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
 
 const connectDB = () => {
   mongoose
-    .connect(MONGO_URL, {})
-    .then(() => {
-      console.log("Connected to Mongo successful");
+    .connect(MONGO_URL, {
+      useNewUrlParser: true, // Use the new URL parser (addressing a deprecation warning)
+      useUnifiedTopology: true, // Use the new Server Discovery and Monitoring engine (addressing a deprecation warning)
     })
-    .catch((e) => {
-      console.log(e.message);
+    .then(() => {
+      console.log("Connected to MongoDB successful");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error.message);
     });
 };
+
 module.exports = connectDB;
+
