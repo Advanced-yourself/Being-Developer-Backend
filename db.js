593fc39496e390083eac608ee4e3cd89a4423597
anurag <anuragbadwahe@gmail.com>
2023-11-08 21:06:27 +0530

    build folder added

--- db.js
+++ db.js
@@ -1,38 +1,16 @@
-// const MONGO_URL = process.env.MONGO_URL
-
-// const mongoose = require("mongoose");
-
-// mongoose.set("strictQuery", true);
-
-// const connectDB = () => {
-//   mongoose
-//     .connect(MONGO_URL, {})
-//     .then(() => {
-//       console.log("Connected to Mongo successful");
-//     })
-//     .catch((e) => {
-//       console.log(e.message);
-//     });
-// };
-// module.exports = connectDB;
-
 const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
 
-const connectDB = () => {
-  mongoose
-    .connect(MONGO_URL, {
-      useNewUrlParser: true, // Use the new URL parser (addressing a deprecation warning)
-      useUnifiedTopology: true, // Use the new Server Discovery and Monitoring engine (addressing a deprecation warning)
-    })
-    .then(() => {
-      console.log("Connected to MongoDB successful");
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB:", error.message);
-    });
+const connectDB = async () => {
+  try{
+    await mongoose.connect(MONGO_URL);
+      console.log('MongoDB connected');
+  }
+  catch(error){
+    console.error("Error connecting to MongoDB:", error.message);
+  };
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
+const path = require("path");
+const { fileURLToPath } =  require("url");
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = require("./db")
 const app = express();
 const cors = require("cors");
-const path = require("path");
 const PORT = process.env.PORT;
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/auth");
@@ -14,11 +15,20 @@ const sheetsRouter = require("./routes/sheets");
 const topicRouter = require("./routes/topics");
 const questionRouter = require("./routes/question");
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 // Cors policy
 app.use(cors());
 
 //using middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./", "build", "index.html"));
+});
+
 
 app.use('/api/auth', userRouter);
 app.use('/api/notes', notesRouter);
@@ -27,7 +37,7 @@ app.use("/api/topics", topicRouter);
 app.use('/api/questions', questionRouter);
 
 
-app.listen(PORT, () => {
-  connectDB();
+app.listen(PORT, 
+  async () =>{ await connectDB()
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
